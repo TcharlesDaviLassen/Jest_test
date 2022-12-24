540d289e20319340fed8d54fbd6efcfe5bdf9c0d
TcharlesDaviLassen <tcharlesdavilassen@gmail.com>
2022-12-24 20:17:42 -0300

    Test complement

--- sumTest.ts
+++ sumTest.ts
@@ -1,3 +1,21 @@
 export const sum = (a: number, b: number) => {
     return a + b;
 }
+
+// Test two
+export const myBeverage = {
+    delicious: true,
+    sour: false,
+};
+
+
+// Teste Binario com função regular 
+export const binaryStringToNumber = (binString: any) => {
+
+    if (!/^[01]+$/.test(binString)) {
+        throw new Error('Not a binary number.');
+
+    }
+
+    return parseInt(binString, 2);
+};
--- test.ts
+++ test.ts
@@ -1,7 +1,53 @@
-import { sum } from './sumTest';
+import { error } from 'console';
+import { sum, myBeverage, binaryStringToNumber } from './sumTest';
 
+// Implementações jest para testes unitários.
 describe('sum module', () => {
-    test('adds 1 + 2 to equal 3', () => {
+
+    it('adds 1 + 2 to equal 3', () => {
         expect(sum(1, 2)).toBe(3);
     });
+
+    it('adds 2 + 3 not to equal 6', () => {
+        expect(sum(2, 3)).not.toEqual(6)
+
+    });
+
 });
+
+// myBeverage
+describe('my beverage', () => {
+    test('is delicious', () => {
+        expect(myBeverage.delicious).toBeTruthy();
+    });
+
+    test('is not sour', () => {
+        expect(myBeverage.sour).toBeFalsy();
+    });
+});
+
+
+// binaryStringToNumber
+describe('binaryStringToNumber', () => {
+
+    describe('dada uma string binária inválida', () => {
+
+      test('composto de não-números lança um Error', () => {
+        expect(() => binaryStringToNumber('abc')).toThrow();
+      });
+  
+      test('com espaço em branco extra lança Error', () => {
+        expect(() => binaryStringToNumber(' 100')).toThrow();
+      });
+    });
+  
+    describe('dada uma string binária válida', () => {
+      test('retorna o número correto', () => {
+        expect(binaryStringToNumber('100')).toBe(4);
+      });
+    });
+  });
+
+
+
+  
